fa05249fcb3c232699be6224ab01431478c69a84
junyichen <junyichen@junyichen-MS-7808>
2017-04-02 10:17:06 +0800

    WordDoc job3

--- src/WordCountPerDoc.java
+++ src/WordCountPerDoc.java
@@ -20,9 +20,6 @@ public class WordCountPerDoc {
     public static class TokenizerMapper
        extends Mapper<Object, Text, Text, Text>{
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-
     public void map(Object key, Text value, Context context
                     ) throws IOException, InterruptedException {
     	/*
